100df5b83484d65d10f790cccfdaa520f0815ff9
Arshdeep Singh <arshdeepsingh@Arshdeeps-MacBook-Pro.local>
2017-08-17 15:48:14 +0530

    Initial Commit

---
+++ LinkUs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LinkUs
+//
+//  Created by Arshdeep Singh on 17/08/17.
+//  Copyright © 2017 Arshdeep Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LinkUs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LinkUs
+//
+//  Created by Arshdeep Singh on 17/08/17.
+//  Copyright © 2017 Arshdeep Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
